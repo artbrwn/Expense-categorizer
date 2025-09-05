766498d15eed4869e0ea6585820d40feb8487bbb
Arturo <md.arturo@gmail.com>
2025-09-05 11:51:28 +0200

    Add tests for LoadData class

---
+++ tests/test_load_data.py
@@ -0,0 +1,12 @@
+import pytest
+from src.load_data import LoadData
+
+def test_non_existing_folder_fails():
+    with pytest.raises(FileNotFoundError) as file_exception:
+        load_data = LoadData("x")
+        assert file_exception == "No folder exists at x path"
+
+def test_valid_folder_does_not_fail():
+    load_data = LoadData("statements")
+    assert load_data.folder_path == "statements"
+    
