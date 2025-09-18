19d333997e5dc7415a5ff682d989ba496f33600f
Arturo <md.arturo@gmail.com>
2025-09-18 17:09:12 +0200

    Add methods from models to review

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,36 @@ def index():
 
 @app.route("/review")
 def review():
-    return render_template("review.html", transactions={}, categories={})
+    from app.models.normalize_data import NormalizeData
+    from app.models.load_data import LoadData
+    from app.models.categorize_transactions import CategorizeTransactions
+
+    # Create instance of LoadData for statements folder
+    load_data = LoadData("statements")
+
+    # Extract text from all pdf's in folder
+    raw_text = load_data.extract_all_texts()
+
+    # Create instance of NormalizeData for the extracted text
+    normalize_data = NormalizeData(raw_text)
+
+    # Get only transactions from text
+    transactions_list = normalize_data.extract_transactions()
+
+    # Format extracted transactions
+    transformed_list = normalize_data.transform_transactions()
+
+    # Apply correct signs to transactions
+    normalize_data.apply_signs()
+
+    # Create instance of CategorizeTransactions
+    categorize_transactions = CategorizeTransactions()
+
+    # Load config from json file
+    categorize_transactions.load_config()
+
+    # First categorization of data
+    categorize_transactions.categorize(normalize_data.transactions)
+    all_transactions = categorize_transactions.categorized_transactions
+    all_categories = categorize_transactions.categories
+    return render_template("review.html", transactions=all_transactions, categories=all_categories)
