888575464407b185c54e8b4ec4ef146bc5fa87a4
Arturo <md.arturo@gmail.com>
2025-09-18 15:01:24 +0200

    Imrpove structure for review html

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/review")
+def review():
+    return render_template("review.html", transactions={}, categories={})
