aabb47c81fb4036a3d205ff9531061cdb2f27e44
Arturo <md.arturo@gmail.com>
2025-11-01 19:32:55 +0100

    Create process endpoint to separate logic from review

--- app/routes.py
+++ app/routes.py
@@ -3,16 +3,37 @@ from flask import render_template, request, redirect
 from app.models.transaction_processor import TransactionProcessor
 import json
 from app.models.analyze_transactions import AnalyzeTransactions
+from datetime import datetime
+from pathlib import Path
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/process")
+def process():
+    message = None
+    try:
+        processor = TransactionProcessor()
+        all_transactions = processor.process_transactions()
+        
+        timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M")
+        filename = f"transactions_{timestamp}.json"
+        save_dir = Path("data/auto_processed")
+        save_dir.mkdir(parents=True, exist_ok=True)
+
+        with open("data/auto_processed/" + filename, "w") as file:
+            json.dump(all_transactions, file, default=str)
+    except Exception as e:
+        message = f"An error has occurred: {e}"
+    
+    return render_template("process.html", message=message)
+    
+
 @app.route("/review", methods=["GET", "POST"])
 def review():
     processor = TransactionProcessor()
-    all_transactions = processor.process_transactions("statements")
-
+    all_transactions = processor.process_transactions()
     if request.method == "GET":
         all_categories = processor.categories
         return render_template("review.html", 
